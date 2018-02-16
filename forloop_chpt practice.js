d34b690097a26471698f6e25f1748b7cf3c6a6bf
Phillip Tyler <ptylerjr2@gmail.com>
2018-02-16 00:16:44 -0500

    a program for a problem at the end of chapter 2 for "Eloquent JavaScript"

---
+++ forloop_chpt practice.js
@@ -0,0 +1,33 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+/*this is a for loop practice problem */
+/* you have to make the "#" mark appear 7 times startign from one */
+
+//making the variable to hold the "#"
+
+var hash="";
+///for loop to generate numbers 1 to 7
+for(var i=0; i<8; i++){
+  ///adding a plus 1 to hash for each number 
+  hash += "#";
+  console.log(hash);///outputting result
+}
+
+/* output 
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/ 
