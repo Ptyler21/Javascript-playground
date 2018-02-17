b17ca4427f94369a88c4aaf1cf4dc990b4ff917a
Phillip Tyler <ptylerjr2@gmail.com>
2018-02-16 21:40:41 -0500

    done with FizzBuzz Problem, chapter 2

--- fizz_Buzz.js
+++ fizz_Buzz.js
@@ -1,25 +1,28 @@
-/*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
+/* Write a program that uses console.log to print all the numbers from 1 to 100, 
+with two execptions. For numbers divisible by 3, print "Fizz" instead of the number,
+and for numbers divisible by 5(and not 3), print "Buzz" instead. 
 
+When you have that working,modify your program to print "FizzBuzz" , for 
+numbers that are divisible by 3 and 5. 
+*/
+///making 0 - 100
 for(var i =0; i < 101; i++){
-if(i % 3 == 0){
-  console.log("FIZZ");
+  /* if number divisible by 3... */
+ if(i % 3 ==0){
+   console.log("Fizz"); //print Fizz
   }
+ /* if number divisible by 5...*/
 else if(i % 5 == 0){
-    console.log("BUZZ");
-  }
-else if(i == (i % 3 == 0) && (i % 5 == 0)){
-    
-    console.log("FIZZBUZZ");
-   }
+ console.log("Buzz"); ///print buzz 
+}
 
-else
+var f = i % 3 == 0; ///storing variables divisble by 3
+var b = i % 5 == 0; ///storing variable divisble by 5 
+
+/* if i equals f and b then print FizzBuzz */
+if (f && b){
+ console.log("FizzBuzz");
+}
+else 
 console.log(i);
-
 }
